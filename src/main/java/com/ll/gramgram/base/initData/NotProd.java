280477ada52640ac648900ebd84c53c567f5ecf5
Heeesuu <gmltn0508@icloud.com>
2023-05-08 16:46:19 +0900

    내가 받은 호감리스트에 기본데이터 추가

--- src/main/java/com/ll/gramgram/base/initData/NotProd.java
+++ src/main/java/com/ll/gramgram/base/initData/NotProd.java
@@ -50,6 +50,8 @@ public class NotProd {
                 Ut.reflection.setFieldValue(likeablePersonToinstaUser4, "modifyUnlockDate", LocalDateTime.now().minusSeconds(1));
                 LikeablePerson likeablePersonToinstaUser100 = likeablePersonService.like(memberUser3, "insta_user100", 2).getData();
                 Ut.reflection.setFieldValue(likeablePersonToinstaUser100, "modifyUnlockDate", LocalDateTime.now().minusSeconds(1));
+                LikeablePerson likeablePersonToSsuehee = likeablePersonService.like(memberUser5, "ssuehee", 1).getData();
+                LikeablePerson likeablePersonToSsuehee2 = likeablePersonService.like(memberUser4, "ssuehee", 1).getData();
             }
         };
     }
